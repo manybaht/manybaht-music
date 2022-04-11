b2f74c2d0e67d11b5a3ad3235747bb939f3fa9cc
Teerut26 <teerutsr@gmail.com>
2022-04-11 22:54:36 +0700

    fix push command global

--- example3/push_global.js
+++ example3/push_global.js
@@ -18,7 +18,7 @@ const rest = new REST({ version: '9' }).setToken(token);
 		console.log('Started refreshing application (/) commands.');
 
 		await rest.put(
-			Routes.applicationGuildCommands(clientId),
+			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
